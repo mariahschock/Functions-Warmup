f214e97fee864dc1b44503207c3bba43724c7c39
Mariah <mariahschock@gmail.com>
2022-04-27 10:45:13 -0700

    answers 1-5

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const colors = ['red', 'blue', 'green', 'yellow']
 
 console.log("============ Question 1 ============")
 // Using its index value in colors, console.log 'blue'
+console.log(colors[1]);
 
 console.log("============ Question 2 ============")
 // add the value 'purple' to the end of the colors array
 // console.log the result
+colors.push("purple");
+console.log(colors[4]);
 
 const benny = {
   name: 'Benny',
@@ -15,17 +18,24 @@ const benny = {
 
 console.log("============ Question 3 ============")
 // Using dot notation, console.log the name and age of Benny
+console.log(benny.name, benny.age);
 
 console.log("============ Question 4 ============")
 // Using dot notation, console.log any one of Benny's breeds
+console.log(benny.breeds[0]);
+for (let breed of benny.breeds) {
+  console.log(breed);
+}
 
 console.log("============ Question 5 ============")
 // Add code to the whisper function so that it takes whatever string is given to it
 // and returns an all lower case version
 
 function whisper(word){
-  // Your code here
+  return word.toLowerCase();
 }
+  // Your code here
+
 
 console.log(`this should print true: ${whisper('Hello') === 'hello'}`)
 console.log(`this should print true: ${whisper('BOOGERS') === 'boogers'}`)
@@ -50,6 +60,7 @@ const dogs = [{
 }]
 console.log("============ BONUS -- 1 ============")
 // Using index values and dot notation, console.log each dog's name
+console.log();
 
 console.log("============ BONUS -- 2 ============")
 // Using index values and dot notation, console.log the first breed of each dog
